0d535a9326cf250c5b0b25175b3ab90ca44ab7ad
Yin Jie <50438365+yjie2366@users.noreply.github.com>
2021-12-09 21:27:55 +0900

    Update conf.py
    Modify project name and copyright information.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'DTF-doc'
+copyright = '2021, NII-Syssoft'
+author = 'NII-Syssoft'
 
 release = '0.1'
 version = '0.1.0'
